84f085402ff29ae365968d6b14047cab06c28ccd
Mathias Hillesheim <mathiashillesheim@gmail.com>
2021-03-20 01:04:07 -0300

    Add collision system with new enemy respawn and score

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 import pygame
 import random
 
@@ -29,6 +30,9 @@ ENEMY_02_IMAGE = "images/plastic_bottle.png"
 ENEMY_03_IMAGE = "images/straw_01.png"
 ENEMY_04_IMAGE = "images/straw_02.png"
 
+# Collision constants
+COLLISION_DISTANCE = 35
+
 
 def setup_screen():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -38,9 +42,37 @@ def setup_screen():
     return screen
 
 
+def generate_enemy():
+    """ Place enemy in a new location and choose a new random skin for it """
+    enemy_skin = random.choice(
+        [ENEMY_01_IMAGE, ENEMY_02_IMAGE, ENEMY_03_IMAGE, ENEMY_04_IMAGE]
+    )
+
+    global ENEMY
+    global ENEMY_POSITION_X
+    global ENEMY_POSITION_Y
+    ENEMY = pygame.image.load(enemy_skin)
+    ENEMY_POSITION_X = random.randint(SCREEN_LEFT_BOUNDARY, SCREEN_RIGHT_BOUNDARY)
+    ENEMY_POSITION_Y = random.randint(SCREEN_TOP_BOUNDARY, SCREEN_ENEMY_BOTTOM_BOUNDARY)
+
+
 def update_player(x, y):
     """ Update position of player image on main screen """
     MAIN_SCREEN.blit(PLAYER, (x, y))
+
+
+def fix_player_bounds():
+    """ Fix player bounds to don't let it slip off the screen """
+    global PLAYER_POSITION_X
+    global PLAYER_POSITION_Y
+    if PLAYER_POSITION_X < SCREEN_LEFT_BOUNDARY:
+        PLAYER_POSITION_X = SCREEN_LEFT_BOUNDARY
+    if PLAYER_POSITION_X > SCREEN_RIGHT_BOUNDARY:
+        PLAYER_POSITION_X = SCREEN_RIGHT_BOUNDARY
+    if PLAYER_POSITION_Y < SCREEN_PLAYER_TOP_BOUNDARY:
+        PLAYER_POSITION_Y = SCREEN_PLAYER_TOP_BOUNDARY
+    if PLAYER_POSITION_Y > SCREEN_BOTTOM_BOUNDARY:
+        PLAYER_POSITION_Y = SCREEN_BOTTOM_BOUNDARY
 
 
 def update_enemy(x, y):
@@ -64,7 +96,18 @@ def reload_bullet():
     BULLET_POSITION_Y = PLAYER_DEFAULT_POSITION_Y
 
 
+def objects_collide(object_A_X, object_A_Y, object_B_X, object_B_Y):
+    """ Calculate the collision between two given objects """
+    X_difference = object_A_X - object_B_X
+    Y_difference = object_A_Y - object_B_Y
+    distance = math.sqrt(math.pow(X_difference, 2) + math.pow(Y_difference, 2))
+    if distance < COLLISION_DISTANCE:
+        return True
+    return False
+
+
 pygame.init()
+print("=> Welcome to SEA INVADERS! You better hit these bastards with that crab!")
 
 # Main screen
 MAIN_BACKGROUND = pygame.image.load(SCREEN_BACKGROUND)
@@ -73,9 +116,12 @@ MAIN_SCREEN = setup_screen()
 
 # Main character (player)
 PLAYER = pygame.image.load(PLAYER_IMAGE)
+PLAYER_SCORE = 0
 PLAYER_POSITION_X = PLAYER_DEFAULT_POSITION_X
 PLAYER_POSITION_Y = PLAYER_DEFAULT_POSITION_Y
 PLAYER_POSITION_X_CHANGE = 0
+PLAYER_POSITION_Y_CHANGE = 0
+PLAYER_POSITION_X_PACE = 5
 
 # Bullet (crab lol)
 BULLET = pygame.image.load(BULLET_IMAGE)
@@ -83,14 +129,15 @@ BULLET_STATE = BULLET_STATE_READY
 BULLET_POSITION_X = 0
 BULLET_POSITION_Y = PLAYER_DEFAULT_POSITION_Y
 BULLET_POSITION_X_CHANGE = 0
-BULLET_POSITION_Y_CHANGE = 0.4
+BULLET_POSITION_Y_CHANGE = 3
 
 # Enemy
-ENEMY = pygame.image.load(ENEMY_04_IMAGE)
-ENEMY_POSITION_X = random.randint(SCREEN_LEFT_BOUNDARY, SCREEN_RIGHT_BOUNDARY)
-ENEMY_POSITION_Y = random.randint(SCREEN_TOP_BOUNDARY, SCREEN_ENEMY_BOTTOM_BOUNDARY)
-ENEMY_POSITION_X_CHANGE = 0.1
-ENEMY_POSITION_Y_CHANGE = 40
+ENEMY = None
+ENEMY_POSITION_X = None
+ENEMY_POSITION_Y = None
+ENEMY_POSITION_X_PACE = 1
+ENEMY_POSITION_Y_PACE = 40
+generate_enemy()
 
 run = True
 while run:
@@ -98,14 +145,15 @@ while run:
     MAIN_SCREEN.blit(MAIN_BACKGROUND, (0,0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            print(f"=> Hope I can see you soon. Your score was {PLAYER_SCORE}!")
             run = False
 
         # Move PLAYER
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                PLAYER_POSITION_X_CHANGE = -0.2
+                PLAYER_POSITION_X_CHANGE = -PLAYER_POSITION_X_PACE
             if event.key == pygame.K_RIGHT:
-                PLAYER_POSITION_X_CHANGE = 0.2
+                PLAYER_POSITION_X_CHANGE = PLAYER_POSITION_X_PACE
             if event.key == pygame.K_SPACE and BULLET_STATE is BULLET_STATE_READY:
                 BULLET_POSITION_X = PLAYER_POSITION_X
                 fire_bullet(PLAYER_POSITION_X, BULLET_POSITION_Y)
@@ -116,25 +164,18 @@ while run:
     PLAYER_POSITION_X += PLAYER_POSITION_X_CHANGE
 
     # Fix player boundaries
-    if PLAYER_POSITION_X < SCREEN_LEFT_BOUNDARY:
-        PLAYER_POSITION_X = SCREEN_LEFT_BOUNDARY
-    if PLAYER_POSITION_X > SCREEN_RIGHT_BOUNDARY:
-        PLAYER_POSITION_X = SCREEN_RIGHT_BOUNDARY
-    if PLAYER_POSITION_Y < SCREEN_PLAYER_TOP_BOUNDARY:
-        PLAYER_POSITION_Y = SCREEN_PLAYER_TOP_BOUNDARY
-    if PLAYER_POSITION_Y > SCREEN_BOTTOM_BOUNDARY:
-        PLAYER_POSITION_Y = SCREEN_BOTTOM_BOUNDARY
+    fix_player_bounds()
 
     # Move enemy
-    ENEMY_POSITION_X += ENEMY_POSITION_X_CHANGE
+    ENEMY_POSITION_X += ENEMY_POSITION_X_PACE
     if ENEMY_POSITION_X > SCREEN_RIGHT_BOUNDARY:
-        ENEMY_POSITION_X_CHANGE = -ENEMY_POSITION_X_CHANGE
+        ENEMY_POSITION_X_PACE = -ENEMY_POSITION_X_PACE
         ENEMY_POSITION_X = SCREEN_RIGHT_BOUNDARY
-        ENEMY_POSITION_Y += ENEMY_POSITION_Y_CHANGE
+        ENEMY_POSITION_Y += ENEMY_POSITION_Y_PACE
     if ENEMY_POSITION_X < SCREEN_LEFT_BOUNDARY:
-        ENEMY_POSITION_X_CHANGE = -ENEMY_POSITION_X_CHANGE
+        ENEMY_POSITION_X_PACE = -ENEMY_POSITION_X_PACE
         ENEMY_POSITION_X = SCREEN_LEFT_BOUNDARY
-        ENEMY_POSITION_Y += ENEMY_POSITION_Y_CHANGE
+        ENEMY_POSITION_Y += ENEMY_POSITION_Y_PACE
 
     # Crab movement
     if BULLET_STATE is BULLET_STATE_RELOADING:
@@ -143,6 +184,15 @@ while run:
         if BULLET_POSITION_Y < SCREEN_TOP_BOUNDARY:
             reload_bullet()
 
+    # Collision check
+    collision = objects_collide(
+        ENEMY_POSITION_X, ENEMY_POSITION_Y, BULLET_POSITION_X, BULLET_POSITION_Y
+    )
+    if collision:
+        reload_bullet()
+        PLAYER_SCORE += 1
+        print(f"=> Hit! Your score is {PLAYER_SCORE}")
+        generate_enemy()
 
 
     update_player(PLAYER_POSITION_X, PLAYER_POSITION_Y)
